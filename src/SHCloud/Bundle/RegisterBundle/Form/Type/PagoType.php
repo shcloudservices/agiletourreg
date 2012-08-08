10cec216189055b884f410bca4a0ce2f583c8b1a
Adrian Moya <adrianmoya@gmail.com>
2012-08-07 21:22:40 -0430

    Funcionando el formulario

--- src/SHCloud/Bundle/RegisterBundle/Form/Type/PagoType.php
+++ src/SHCloud/Bundle/RegisterBundle/Form/Type/PagoType.php
@@ -2,11 +2,12 @@
 namespace SHCloud\Bundle\RegisterBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class PagoType extends AbstractType{
 
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         
     }
